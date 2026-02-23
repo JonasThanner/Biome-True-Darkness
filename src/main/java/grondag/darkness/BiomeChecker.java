4d530016d43356ef4e08a9aa9f828e01a518dc3b
JonasThanner <94178649+JonasThanner@users.noreply.github.com>
2026-02-23 23:29:05 +0100

    Fix Crash coming from BiomeWeAreIn being null

--- src/main/java/grondag/darkness/BiomeChecker.java
+++ src/main/java/grondag/darkness/BiomeChecker.java
@@ -10,6 +10,7 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.biome.Biome;
+import net.minecraftforge.registries.ForgeRegistries;
 
 import java.util.Properties;
 
@@ -32,12 +33,22 @@ public class BiomeChecker {
         //Check if the Biome Should be dark
         //Get Biome string
         ResourceLocation biomeWeAreIn = world.registryAccess().registryOrThrow(Registries.BIOME).getKey(BiomeChecker.GetCurrentBiome());
+        ResourceLocation forgeBiomeWeAreIn = ForgeRegistries.BIOMES.getKey(BiomeChecker.GetCurrentBiome()); //undermod registers it's biomes in the forge BIOME registry rather than the vanilla biome registry, as such we must check there as well
         JsonArray array = Darkness.darknessBiomes.getAsJsonArray("Biomes");
         boolean insideSpecifiedBiome = false;
+
         for (JsonElement element : array) {
-            if (element.getAsString().equals(biomeWeAreIn.toString())) {
-                insideSpecifiedBiome = true;
-                break;
+            if (biomeWeAreIn != null) {
+                if (element.getAsString().equals(biomeWeAreIn.toString())) {//check vanilla's biome registry for the current biome
+                    insideSpecifiedBiome = true;
+                    break;
+                }
+            }
+            if (forgeBiomeWeAreIn != null) { //check forge's biome registry for the current biome
+                if (element.getAsString().equals(forgeBiomeWeAreIn.toString())) {
+                    insideSpecifiedBiome = true;
+                    break;
+                }
             }
         }
 
